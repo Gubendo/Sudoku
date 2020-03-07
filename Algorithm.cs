f2dabdf04b0181bae12c9f344f412c0eb6c29502
Quentin Pichollet <quentin.pichollet@ensiie.fr>
2020-03-07 16:48:33 -0500

    fix MRV/DH

--- Algorithm.cs
+++ Algorithm.cs
@@ -70,44 +70,46 @@ namespace IA_TP2
         /**
          * On selectionne la variable qui a le moins de valeurs légales et on renvoie sa position i,j
          */
-        public static (int, int) selectMRV(Sudoku sudoku)
+        public static List<(int, int)> selectMRV(Sudoku sudoku)
         {
             int i = 0;
             int j = 0;
+            List<(int,int)> ret = new List<(int,int)>();
 
             for (int indexI = 0; i < sudoku.size; i++)
             {
                 for (int indexJ = 0; j < sudoku.size; j++)
                 {
-                    if (sudoku.mySudoku[i][j].domain.Count > sudoku.mySudoku[indexI][indexJ].domain.Count)
+                    if (sudoku.mySudoku[i][j].domain.Count >= sudoku.mySudoku[indexI][indexJ].domain.Count)
                     {
                         i = indexI;
                         j = indexJ;
+                        ret.Add((i,j));
                     }
                 }
             }
-            return (i, j);
+            return ret;
         }
 
         /**
          * On selectionne la variable qui contraint le plus de variables et on renvoie sa position i,j
+         * On appelle selectDH après avoir appelé selectMRV
          */
-        public static (int, int) selectDH(Sudoku sudoku)
+        public static (int, int) selectDH(Sudoku sudoku,List<(int,int)> resultMRV)
         {
             int i = 0;
             int j = 0;
-
-            for (int indexI = 0; i < sudoku.size; i++)
+            (int,int) current;
+            int length = resultMRV.Count;
+            for (int k = 0; k < length; k++)
             {
-                for (int indexJ = 0; j < sudoku.size; j++)
-                {
-                    if (sudoku.mySudoku[i][j].getRelatives().Count < sudoku.mySudoku[indexI][indexJ].getRelatives().Count)
-                    {
-                        i = indexI;
-                        j = indexJ;
-                    }
-                }
-            }
+               current = resultMRV.ElementAt(k);
+               if (sudoku.mySudoku[i][j].getRelatives().Count < sudoku.mySudoku[current.Item1][current.Item2].getRelatives().Count)
+               {
+                   i = current.Item1;
+                   j = current.Item2;
+               }
+            }            
             return (i, j);
         }
 
@@ -133,6 +135,18 @@ namespace IA_TP2
             return (i, j);
         }
 
+        public static Sudoku backtracking(Sudoku sudoku)
+        {
+            // 1 - Selectionner var non assigné : utiliser MRV puis DH en cas d'égalité
+            (int , int) var;
+            var = selectDH(sudoku,selectMRV(sudoku));
+
+            // 2 - Selectionner val pour var choisie : utiliser LCV
+            // 3 - AC3 ?
+            // 4 - Recursivité
+
+            return sudoku;
+        }
 
 
 
